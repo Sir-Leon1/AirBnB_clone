04fdb937668b357b4cdd2a5828636f9d6e4ceb7c
Sir-Leon <eb3.61594.22@student.chuka.ac.ke>
2024-05-19 17:43:43 +0300

    Console first push

---
+++ console.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Defines the HBNB console."""
+import cmd
+import re
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt
+    """
+    prompt = "(hbnb) "
+    __classes = {
+            "BaseModel",
+            "User",
+            "State",
+            "City",
+            "Place",
+            "Amenity",
+            "Review"
+            }
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+    def default(self, arg):
+        """Default behaviour for cmd module when input is invalid."""
+        argdict = {
+                "all": self.do_all,
+                "show": self.do_show,
+                "destroy": self.do_destroy,
+                "count": self.do_count,
+                "update": self.do_update
+                }
+
+        match = re.search(r"\.", arg)
+        if match is not None:
+            arg1 = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((,*?)\)", arg1[1])
+            if match is not None:
+                command = [arg1[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(arg1[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Eof signal to exit the program."""
+        print("")
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
